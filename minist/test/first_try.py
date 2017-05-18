f8f1b4e90192fb44452202a77af36775369575cb
will bai <549436901@qq.com>
2017-05-18 17:21:51 +0800

    l

--- minist/test/first_try.py
+++ minist/test/first_try.py
@@ -1,3 +1,17 @@
 import input_data
+import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder("float", [None, 784])
+cross_entropy = - tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDesentOptimizer(0.01).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+for i in range(1000):
+    batch_xs, batch_ys, = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
