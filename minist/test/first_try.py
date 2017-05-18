efc4ef9f20cca1d74b87eccc7c2f20de0b22ee7f
will bai <549436901@qq.com>
2017-05-18 18:31:21 +0800

    first_stable

--- minist/test/first_try.py
+++ minist/test/first_try.py
@@ -7,17 +7,17 @@ x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder("float", [None, 784])
+y_ = tf.placeholder("float", [None, 10])
 
 cross_entropy = - tf.reduce_sum(y_ * tf.log(y))
-train_step = tf.train.GradientDesentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
-    batch_xs, batch_ys, = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
